ce0c347c7f950bf3e5a3d2db3039c5d5349e6d74
dogia <dogia@github.com>
2021-10-10 18:32:36 -0500

    primer commit

---
+++ src/views/contacto.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="primero">
+    <NavBar/>
+
+  <div class="container mt-5">
+    <section id="section1">
+        <h1 class="mb-4">Nuestro equipo de trabajo</h1>
+        <b-row>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Daniela Zuluaga Tamayo"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                <b>Frontend Dev</b>
+                <br/>
+                danizuta94@gmail.com
+                <br/>
+                +57 300 730 13 57
+              </b-card-text>
+            </b-card>
+          </b-col>
+
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Iván Salas Méndez"
+              img-src="https://picsum.photos/600/300/?image=26"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                <br/>
+                <b>Arquitecto</b>
+                <br/>
+                ivanruggieris@gmail.com
+                <br/>
+                +57 304 772 27 86
+              </b-card-text>
+            </b-card>
+          </b-col>
+
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Daniel Eduardo Medina G."
+              img-src="https://picsum.photos/600/300/?image=27"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                <b>Backend Dev</b>
+                <br/>
+                daedmegu@gmail.com
+                <br/>
+                +57 319 626 90 75
+              </b-card-text>
+            </b-card>
+          </b-col>
+
+
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Alexander Díaz Garzón"
+              img-src="https://picsum.photos/600/300/?image=28"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                <b>Analísta</b>
+                <br/>
+                alexdiazgar@misena.edu.co
+                <br/>
+                +57 301 336 12 44
+              </b-card-text>
+            </b-card>
+          </b-col>
+
+
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Kevin Andrés Gamboa G."
+              img-src="https://picsum.photos/600/300/?image=29"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                <b>Tester</b>
+                <br/>
+                andresgkevin@unicauca.edu.co
+                <br/>
+                +57 314 403 67 00
+              </b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+      </section>
+      </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/navbar.vue'
+export default {
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      variable: "soy variable",
+
+    }
+  },
+  methods: {
+    guardar() {
+      console.log('ejecutanto...')
+    }
+  },
+}
+</script>
---
+++ src/views/index.vue
@@ -0,0 +1,265 @@
+<template>
+  <div>   
+    <NavBar/>
+    <!-- <nav-bar/> -->
+    <div>
+      <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image"></b-img>
+    </div>
+    <div class="container mt-5">
+      <section id="section1">
+        <h1 class="mb-4">Primera seccion</h1>
+        <b-row>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Primer servicio"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Descripcion primer servicio.
+              </b-card-text>
+
+              <b-button pill>Button</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+      </section>
+      <section id="section2" class="mt-5">
+        <h1 class="mb-4">Segunda sección</h1>
+        <b-row>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+      </section>
+      <section id="section3" class="mt-5">
+        <h1 class="mb-4">Tercera sección</h1>
+        <b-row>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+          <b-col sm="6" md="4" lg="3">
+            <b-card
+              title="Card Title"
+              img-src="https://placekitten.com/1000/300"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+          </b-col>
+        </b-row>
+      </section>
+    </div>
+    <transition name="fade">
+      <a v-if="!isVisibleUpButton" href="#" class="back-to-top"><b-icon-arrow-up-circle-fill font-scale="3"></b-icon-arrow-up-circle-fill></a>
+    </transition>
+    <footer class="d-flex bg-dark text-light align-items-center px-3 py-2">
+      <div class="text-center w-100">
+        <strong class="mr-auto">Footer</strong>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+import NavBar from '../components/navbar.vue'
+export default {
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      isVisibleUpButton: true,
+      scrollPosition: 0,
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.scrollPosition = window.scrollY
+    }
+  },
+  watch: {
+    scrollPosition() {
+      if (this.scrollPosition > 100) {
+        this.isVisibleUpButton = false;
+      } else {
+        this.isVisibleUpButton = true;
+      }
+    }
+  },
+}
+</script>
+<style>
+.back-to-top {
+  position: fixed;
+  right: 15px;
+  bottom: 15px;
+  z-index: 99999;
+}
+</style>
